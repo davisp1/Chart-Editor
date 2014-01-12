f5aa6a24eaa294fb02e979f1ca8b22231c7b835d
Julien Marinescu <davisp@xenbox.fr>
2014-01-12 14:19:58 +0100

    user can upload a .csv file

--- js/editor.js
+++ js/editor.js
@@ -2,18 +2,20 @@ $(function() {
     oStorage  = new editor.MyStorage();
     json_data  = oStorage.getItem("json_data");
     editor.update_data();
-    
-    $(Object.keys(json_data)).each(function(){
-        if($("#pieLabel option").length===0){
-            var titles = json_data[this].titles;
-            var option1 = $("<option/>").attr("value",titles[0]).text(titles[0]);
-            var option2 = $("<option/>").attr("value",titles[1]).text(titles[1]);
-            $("#pieLabel").append(option1);
-            $("#pieNumber").append(option2);
-        }
-        var option = $("<option/>").attr("value",this).text(this);
-        $("#pieSource").append(option);
-    });
+    if(typeof json_data != "undefined" && json_data!=null){
+        $(Object.keys(json_data)).each(function(){
+            if($("#pieLabel option").length===0){
+                var titles = json_data[this].titles;
+                var option1 = $("<option/>").attr("value",titles[0]).text(titles[0]);
+                var option2 = $("<option/>").attr("value",titles[1]).text(titles[1]);
+                $("#pieLabel").append(option1);
+                $("#pieNumber").append(option2);
+            }
+            var option = $("<option/>").attr("value",this).text(this);
+            $("#pieSource").append(option);
+        });
+    }
+
 
     editor.Tab($(".tabs.menu"));
     editor.Tab($(".container_pie"));
@@ -65,7 +67,7 @@ var editor = {
      *
      **/
 
-    Tab : function(target){
+    Tab : function(target, additional){
         
         var object = $(target);
         var container =  $(".content", object);
@@ -75,8 +77,11 @@ var editor = {
             var rel = $(this).attr("rel");
             $(this).parent().find("li").removeClass("active");
             $(this).addClass("active"); 
-            $(container).children().hide();
+            $(container).children().hide().find("li").removeClass('active');
             $("#"+rel).show();
+            if(additional) {
+                additional();
+            }
         }
 
         $("li", header).click(onClickTab);
@@ -97,6 +102,29 @@ var editor = {
             var title = record.title;
             
             $("#dataTextarea").val(JSON.stringify(data, null, "\t"));
+            var text = "";
+            for (var i = 0; i < data.length; i++) {
+                var record = data[i];
+                var line = "";
+                for (var j = 0; j < record.length; j++) {
+                    var value = record[j]
+                    if(j==0){
+                        line = value;
+                    }
+                    else{
+                        line = line + "\t" + value;
+                    }
+                };
+                if(i==0){
+                    text = line
+                }
+                else{
+                    text = text + "\n" + line
+
+                }
+            };
+            $("#dataCopy").val(text); //.val().split("\n").map(function(d){ return d.split("\t") })
+            $("#dataCsv").val("");
             $("#dataTitle").val(title);
             
             var length_column = data[0].length;
@@ -118,24 +146,61 @@ var editor = {
             });
         }
     },
-    
-    computePieForm : function(){
-        var $container = $(".container_pie");
-        
-    },
 
     onSaveData : function(event) {
         event.preventDefault();
-        var titles = $(".new_data thead input").map( function(i,element) { return $(element).val() } );
-        var record = {
-            data : jQuery.parseJSON($("#dataTextarea").val()),
-            title : $("#dataTitle").val(),
-            titles : titles,
+        var titles = $(".new_data thead input");
+        if (titles.length>0)
+            titles = titles.map( function(i,element) { return $(element).val() } );
+        else
+            titles = [];
+        var data_container = $(".panel-collapse.in");
+        var input = data_container.find("input");
+        var textarea = data_container.find("textarea");
+        var value = [];
+        if(input.length>0){
+            var file = input.get(0).files[0];
+            console.log(file);
+                if(typeof file != "undefined"){
+                    var reader = new FileReader();
+                    reader.onload = function(e) { 
+                        var result = e.target.result;
+                        value = d3.csv.parseRows(result.replace(/\s*;/g, ","));
+                        var record = {
+                            data : value,
+                            title : $("#dataTitle").val(),
+                            titles : titles,
+                        }
+                        editor.save_data(record);
+                    }; 
+                    reader.readAsText(file);
+                    //
+                    return false;
+                }
+                else{
+                    alert("error");
+                    return false
+                }
+
         }
-        
-        editor.save_data(record);
-        
-        window.location.reload();
+        else{
+            if($("#dataTextarea").val()!=""){
+                value = jQuery.parseJSON($("#dataTextarea").val());
+            }
+            else{
+                alert("error t");
+                return false;
+            }
+            console.log(value);
+            var record = {
+                data : value,
+                title : $("#dataTitle").val(),
+                titles : titles,
+            }
+            console.log(record);
+            editor.save_data(record);
+        }
+        //window.location.reload();
     },
     
     onClickData : function(event) {
@@ -160,9 +225,11 @@ var editor = {
 
     update_data : function () {
         var json_data  = oStorage.getItem("json_data");
-        $(Object.keys(json_data)).each(function() {
-                $(".sources").append("<li class='item' rel='"+this+"'><a href='#'>"+this+"</a></li>");
-        });
+        if((typeof json_data !== "undefined") && (json_data!=null)){
+            $(Object.keys(json_data)).each(function() {
+                    $(".sources").append("<li class='item' rel='"+this+"'><a href='#'>"+this+"</a></li>");
+            });
+        }
         $(".sources").append("<li class='item' rel='new'><a href='#'>New</a></li>");
     },
     
@@ -171,18 +238,63 @@ var editor = {
         if((typeof json_data === "undefined") || json_data==null) {
             json_data = {};
         }
+        record.type = "array";
+        if(record.data && !Array.isArray(record.data[0])){
+            record.type = "dict";
+        }
         var json = {
             title : record.title,
             data : record.data,
             titles : record.titles,
+            type : record.type,
         }
+        alert(JSON.stringify(json));
         json_data[record.title] = json;
         oStorage.setItem("json_data",json_data);
+    },
+
+
+    as_dict : function(record){
+        
+        var data = record.data;
+        var result = [];
+        var titles = record.titles;
+
+        if(Array.isArray(data)){
+
+            for (var i = 0; i < data.length; i++) {
+                
+                var item = data[i];
+                var new_item = {};
+                
+                if(Array.isArray(item)){
+                    
+                    for (var i = 0; i < item.length; i++) {
+                        var value = item[i];
+                        var key = "column"+i;
+                        if(titles.length>=(i+1)){
+                            key = titles[i];
+                        }
+                        new_item[key] = value;
+                    }
+                    result.push(new_item)
+                }
+            }
+        }
+    },
+
+    as_array : function(data){
+        if(data.length>0){
+            for (var i = 0; i < data.length; i++) {
+                var item = data[i];
+            }
+        }
     },
 
     Pie : function(container, data, config) {
 
         var container = d3.select(container);
+        container.selectAll("svg").remove();
         var titles =  Array.map(data,function(t){return t[config.label]})
         var radius = +config.radius, padding = 10;
         var color = d3.scale.ordinal()
@@ -217,6 +329,7 @@ var editor = {
                   .style("text-anchor", "middle")
                   .text(config.title);
     },
+
     
     MyStorage : function () {
         try{
