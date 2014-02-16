91b3923879ae649b091b98081bf09cefe0ef3f2b
Julien Marinescu <davisp@xenbox.fr>
2014-02-16 21:35:53 +0100

    controllers related to dataset

---
+++ app/scripts/controllers/dataset.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Controllers */
+
+var datasetControllers = angular.module('DataSetControllers', ['ngRoute',]);
+
+datasetControllers.controller('DataSetListCtrl', ['$scope',
+  function($scope) {
+    console.log($scope);
+  }]);
+
+datasetControllers.controller('DataSetCreateCtrl', ['$scope', '$routeParams', '$rootScope',
+   function($rootScope) {
+    $rootScope.currenDataset = '';
+  }]);
+
+datasetControllers.controller('DataSetEditCtrl', ['$scope', '$routeParams', '$rootScope',
+   function($scope, $routeParams, $rootScope) {
+    $rootScope.currentDataset = $routeParams.datasetId;;
+    $scope.datasetId = $routeParams.datasetId;
+  }]);
