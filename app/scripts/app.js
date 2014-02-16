586b875c8d231aaa9e2e80062aaef87f87f66d56
Julien Marinescu <davisp@xenbox.fr>
2014-02-16 21:37:11 +0100

    routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,6 +5,7 @@ angular.module('newChartEditorApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
+  'DataSetControllers',
   'ui.bootstrap'
 ])
   .config(function ($routeProvider) {
@@ -12,6 +13,14 @@ angular.module('newChartEditorApp', [
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/dataset/create', {
+        templateUrl: 'views/dataset_details.html',
+        controller: 'DataSetCreateCtrl'
+      })
+      .when('/dataset/edit/:datasetId', {
+        templateUrl: 'views/dataset_details.html',
+        controller: 'DataSetEditCtrl'
       })
       .otherwise({
         redirectTo: '/'
