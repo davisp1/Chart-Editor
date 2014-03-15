62cc497a7d80e65dd52a2412cd0a621988119d13
Julien Marinescu <jma@makina-corpus.com>
2014-03-15 15:07:30 +0100

    Updates

---
+++ bower_components/angular-spinner/CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+
+## 0.4.0 - 2014-03-15
+
+- Upgrade spin.js to 2.0.0. See breaking changes [here](http://fgnass.github.io/spin.js/#v2.0.0).
+
+## 0.3.1 - 2014-01-31
+
+- Fixed an issue that caused the minified code to fail.
+
+## 0.3.0 - 2014-01-26
+
+- Add ability to control spinner state with bundled service (([#6](https://github.com/urish/angular-spinner/pull/6), contributed by [lossendae](https://github.com/lossendae))
+
+## 0.2.1 - 2013-08-28
+
+- Add test coverage reporting
+- Stop the spinner on scope destroy
+- Support for AngularJS 1.2
---
+++ bower_components/angular-spinner/karma.conf.js
@@ -0,0 +1,31 @@
+/* License: MIT.
+ * Copyright (C) 2013, 2014, Uri Shaked and contributors.
+ */
+
+'use strict';
+
+module.exports = function (config) {
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine'],
+		logLevel: config.LOG_INFO,
+		browsers: ['PhantomJS'],
+		autoWatch: true,
+		reporters: ['dots', 'coverage'],
+		files: [
+			'components/angular/angular.js',
+			'components/angular-mocks/angular-mocks.js',
+			'components/spin.js/spin.js',
+			'angular-spinner.js',
+			'tests.js'
+		],
+		preprocessors: {
+			'components/spin.js/spin.js': 'coverage',
+			'angular-spinner.js': 'coverage'
+		},
+		coverageReporter: {
+			type: 'lcov',
+			dir: 'coverage/'
+		}
+	});
+};
