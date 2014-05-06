7e5d5a22b41903b8014658cadac0ca220dab3b1c
Julien Marinescu <davisp@xenbox.fr>
2014-05-06 23:17:51 +0200

    improve chart creation page

--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -1,63 +1,84 @@
 'use strict';
 
 angular.module('newChartEditorApp')
-  .controller('ChartCtrl', function ($scope, $routeParams, $window, $rootScope, $localStorage) {
+  .controller('ChartCtrl', function ($scope, $routeParams, $rootScope, $localStorage) {
 
     $rootScope.currentChart = $routeParams.chartId;
     $scope.chartId = $routeParams.chartId;
     $scope.$storage = $localStorage.$default({ charts: {} });
     $scope.datasets = $scope.$storage.datasets;
-
-    $scope.chart = [];
-    $scope.series = [];
-    $scope.title = '';
-    $scope.$window = $window;
-    $scope.example = true;
     $scope.kindCharts = ['bar', 'pie', 'line', 'area', 'point'];
-    $scope.data = [];
-    $scope.dataset = '';
-    $scope.labels = '';
+    $scope.kindLegend = ['lineEnd', 'traditional'];
+    $scope.sharedData = { 
+        dataset: '', 
+        valueColumns: [{ id: '', value: ''}], 
+        titleColumn: '', 
+        chartType: 'bar' 
+      };
 
     if($scope.chartId){
-      $scope.chart = $scope.$storage.datasets[$scope.chartId];
+      $scope.chart = $scope.$storage.charts[$scope.chartId];
       if (typeof($scope.chart) !== 'undefined') {
       }
     }
 
-    $scope.$watch('dataset', function() {
-        $scope.datasetObject= $scope.dataset;
-        $scope.datasetColumns = (angular.isDefined($scope.datasetObject)) ? $scope.datasetObject.titles : [];
-        console.log($scope.datasetColumns);
-      });
-    
+    $scope.config = {
+        title : 'New Chart',
+        tooltips: true,
+        labels : false,
+        mouseover: function() {},
+        mouseout: function() {},
+        click: function() {},
+        legend: {
+          display: true,
+          //could be 'left, right'
+          position: 'right'
+        },
+        colors: ['steelBlue', 'rgb(255,153,0)', 'rgb(220,57,18)', 'rgb(70,132,238)', 'rgb(73,66,204)', 'rgb(0,128,0)'],
+        innerRadius: 0, // Only on pie Charts
+        lineLegend: 'lineEnd', // Only on line Charts
+    };
+
     $scope.data1 = {
-        series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+        series: [],
         data : []
       };
 
     $scope.computeData = function(){
+
         var series = [];
+        var seriesId = [];
+
         var data = [];
-        var labelColumn = $scope.labels;
-        var valueColumn = $scope.values;
-        if (angular.isDefined(labelColumn) && angular.isDefined(valueColumn)) {
-          angular.forEach($scope.dataset.data, function(value, key){
-              console.log(value + key);
+
+        var labelColumn = $scope.sharedData.titleColumn;
+        var valueColumns = $scope.sharedData.valueColumns;
+        var titles = $scope.sharedData.dataset.titles;
+
+        if (angular.isDefined(labelColumn) && valueColumns.length > 0) {
+
+          for (var i = 0; i < valueColumns.length; i++) {
+            var column = valueColumns[i];
+            series.push(column.value);
+            seriesId.push(titles.indexOf(column.id));
+          }
+
+          labelColumn = titles.indexOf(labelColumn);
+
+          angular.forEach($scope.sharedData.dataset.data, function(value){
+              var y = [];
+              for (var i = 0; i < seriesId.length; i++) {
+                console.log(parseFloat(value[seriesId[i]]));
+                y.push(parseFloat(value[seriesId[i]]));
+              }
+              this.push({ x: value[labelColumn]||0, y: y});
             }, data);
+
         }
         $scope.data1 = { series: series, data: data };
       };
 
-    $scope.chartType = 'bar';
-
-    $scope.config = {
-        labels: false,
-        title : 'Not Products',
-        example : $scope.example,
-        legend : {
-            display:true,
-            position:'right'
-          }
-        };
-    angular.element($window).bind('resize',function(e){ console.log(e);$scope.example=!$scope.example;$scope.config.example=$scope.example;});
+    $scope.saveData = function(){
+      $scope.$storage.charts[$scope.chartId] = { config: $scope.config, labels: $scope.labels, values: $scope.values };
+    };
   });
